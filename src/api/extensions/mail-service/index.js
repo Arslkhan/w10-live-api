4c17047eac9b242cd37e7f5a476dfefbc1bf4d33
arslanki5 <arsalan.khan@ki5.co.uk>
2021-09-30 18:55:48 +0500

    contact Us api

--- src/api/extensions/mail-service/index.js
+++ src/api/extensions/mail-service/index.js
@@ -27,7 +27,7 @@ module.exports = ({ config }) => {
         );
         console.log(customerUpdateResponse);
         apiStatus(res, customerUpdateResponse.data);
-        
+
       } catch (error) {
         console.error(error);
         apiStatus(
@@ -57,130 +57,31 @@ module.exports = ({ config }) => {
    * POST send an email
    */
   msApi.post('/send-email', (req, res) => {
-    const userData = req.body
-    if (!userData.token || userData.token !== token) {
-      apiStatus(res, 'Email is not authorized!', 500)
-    }
-    const { host, port, secure, user, pass } = config.extensions.mailService.transport
-    if (!host || !port || !user || !pass) {
-      apiStatus(res, 'No transport is defined for mail service!', 500)
-    }
-    if (!userData.sourceAddress) {
-      apiStatus(res, 'Source email address is not provided!', 500)
-      return
-    }
-    if (!userData.targetAddress) {
-      apiStatus(res, 'Target email address is not provided!', 500)
-      return
-    }
-    // Check if email address we're sending to is from the white list from config
-    const whiteList = config.extensions.mailService.targetAddressWhitelist
-    const email = userData.confirmation ? userData.sourceAddress : userData.targetAddress
-    if (!whiteList.includes(email)) {
-      apiStatus(res, `Target email address (${email}) is not from the whitelist!`, 500)
-      return
-    }
-
-
-    const replyAddress = userData.replyAddress ? userData.replyAddress : ''
-    //const replyAddress = 'clintonjoubert@gmail.com'
-    //const command = ""
-
-    const command = "/var/www/html/sendemail/StockManagement sendemail " + userData.targetAddress + " " + encodeURIComponent(userData.subject) + " '" + encodeURIComponent(userData.emailText) + "' " + replyAddress
-    
-    
-
-    const fs = require('fs');
-    fs.writeFileSync('/var/www/html/sendemail/mail.log', command);
-  
-    const { exec } = require("child_process");
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        apiStatus(res, error, 500)
-          
-          return;
-      }
-      if (stderr) {
-        apiStatus(res, stderr, 500)
-         
-          return;
-      }
-     
-      apiStatus(res, 'OK', 200)
-    });
-  apiStatus(res, 'OK', 200)
-
-
-   /* let transporter = NodeMailer.createTransport({
-      host,
-      port,
-      secure,
-      auth: {
-        user,
-        pass
-      }
-    })
-    const mailOptions = {
-      from: userData.sourceAddress,
-      to: userData.targetAddress,
-      subject: userData.subject,
-      text: userData.emailText
-    }
-    transporter.sendMail(mailOptions, (error) => {
-      if (error) {
-        apiStatus(res, error, 500)
-        return
-      }
-      apiStatus(res, 'OK', 200)
-      transporter.close()
-    })
-    */
-
-
-    // check if provided email addresses actually exist
-    
-    /*
-    EmailCheck(userData.sourceAddress)
-      .then(response => {
-        if (response) return EmailCheck(userData.targetAddress)
-        else {
-          apiStatus(res, 'Source email address is invalid!', 500)
-        }
-      })
-      .then(response => {
-        if (response) {
-          let transporter = NodeMailer.createTransport({
-            host,
-            port,
-            secure,
-            auth: {
-              user,
-              pass
+    try {
+      try {
+        const emailDetails = req.body;
+        const emailResponse = axios.post(
+          'https://secure.w10.world/rest/default/V1/w10/contactus',
+          emailDetails,
+          {
+            headers: {
+              'Content-type': 'application/json'
             }
-          })
-          const mailOptions = {
-            from: userData.sourceAddress,
-            to: userData.targetAddress,
-            subject: userData.subject,
-            text: userData.emailText
           }
-          transporter.sendMail(mailOptions, (error) => {
-            if (error) {
-              apiStatus(res, error, 500)
-              return
-            }
-            apiStatus(res, 'OK', 200)
-            transporter.close()
-          })
-        } else {
-          apiStatus(res, 'Target email address is invalid!', 500)
-        }
-      })
-      .catch(() => {
-        apiStatus(res, 'Invalid email address is provided!', 500)
-      })
-    */
+        );
+        apiStatus(res, emailResponse.data);
+      } catch (error) {
+        console.error(error);
+        apiStatus(
+          res,
+          'This Some Error Occurred while processing contact us email',
+          500
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      apiStatus(res, 'That Some Error Occurred while sending contact us email', 500);
+    }
   })
-
   return msApi
 }
