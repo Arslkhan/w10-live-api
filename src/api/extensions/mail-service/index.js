bc1d5b62da9e0afd18b3704a7e9e15885949b741
arslkhan <arslkhan5@gmail.com>
2021-10-19 01:21:57 +0500

    done

--- src/api/extensions/mail-service/index.js
+++ src/api/extensions/mail-service/index.js
@@ -1,14 +1,56 @@
 import { apiStatus } from "../../../lib/util";
 import { Router } from "express";
-import EmailCheck from "email-check";
+// import EmailCheck from 'email-check'
 import jwt from "jwt-simple";
-import NodeMailer from "nodemailer";
+// import NodeMailer from 'nodemailer'
 import axios from "axios";
 
 module.exports = ({ config }) => {
   const msApi = Router();
   let token;
 
+  /**
+   * GET send token to authorize email
+   */
+  msApi.get("/get-token", (req, res) => {
+    token = jwt.encode(Date.now(), config.extensions.mailService.secretString);
+    apiStatus(res, token, 200);
+  });
+
+  /**
+   * POST send an email
+   */
+  msApi.post("/send-email", (req, res) => {
+    try {
+      try {
+        const emailDetails = req.body;
+        const emailResponse = axios.post(
+          "https://secure.w10.world/rest/default/V1/w10/contactus",
+          emailDetails,
+          {
+            headers: {
+              "Content-type": "application/json",
+            },
+          }
+        );
+        apiStatus(res, emailResponse.data);
+      } catch (error) {
+        console.error(error);
+        apiStatus(
+          res,
+          "This Some Error Occurred while processing contact us email",
+          500
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      apiStatus(
+        res,
+        "That Some Error Occurred while sending contact us email",
+        500
+      );
+    }
+  });
   msApi.post("/update", async (req, res) => {
     try {
       try {
@@ -48,49 +90,6 @@ module.exports = ({ config }) => {
       apiStatus(
         res,
         "That Error Occurred while processing fetching customerUpdateResponse Url",
-        500
-      );
-    }
-  });
-  /**
-   * GET send token to authorize email
-   */
-  // msApi.get('/get-token', (req, res) => {
-  //   token = jwt.encode(Date.now(), config.extensions.mailService.secretString)
-  //   apiStatus(res, token, 200)
-  // })
-
-  /**
-   * POST send an email
-   */
-  msApi.post("/send-email", (req, res) => {
-    try {
-      try {
-        const emailDetails = req.body;
-        const emailResponse = axios.post(
-          "https://secure.w10.world/rest/default/V1/w10/contactus",
-          emailDetails,
-          {
-            headers: {
-              "Content-type": "application/json",
-            },
-          }
-        );
-        console.error("emailResponse", emailResponse);
-        apiStatus(res, emailResponse.data);
-      } catch (error) {
-        console.error(error);
-        apiStatus(
-          res,
-          "This Some Error Occurred while processing contact us email",
-          500
-        );
-      }
-    } catch (error) {
-      console.error(error);
-      apiStatus(
-        res,
-        "That Some Error Occurred while sending contact us email",
         500
       );
     }
